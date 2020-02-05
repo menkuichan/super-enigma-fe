23559bc094e9d8f851798da9170bf31292a30557
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-05 17:42:11 +0300

    code refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './components/Hello/Hello.jsx';
+import Hello from './components/Hello';
 
 ReactDOM.render(
   <Hello />,
