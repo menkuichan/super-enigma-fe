a06d026208434ae8ebfe6f67f1190e8c3efb80df
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-06 16:19:38 +0300

    change search input color and font size

--- src/components/Search/style.js
+++ src/components/Search/style.js
@@ -4,6 +4,8 @@ export const SearchInput = styled.input`
   margin-right: 20px;
   border: none;
   width: 200px;
+  color: #f2f2f2;
+  font-size: 15px;
   background-color: #110F10;
   border-bottom: solid #716F73 2px;
   :focus {
