7438bf92d81bc39946c91e5c33830b084eada781
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-19 16:19:08 +0300

    add image for movies without poster path

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
   },
   plugins: [
