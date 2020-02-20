8a7ede498670cf67c8b3172012e4cfe74c9c2f1a
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-20 17:58:28 +0300

    refactor

--- src/store/sagas/movies.js
+++ src/store/sagas/movies.js
@@ -3,19 +3,26 @@ import moviesApi from '../../api/movies';
 import { MOVIES_PARAMS, NAV_LINKS } from '../../constants';
 import { GET_MOVIES_PENDING, GET_MOVIES_SUCCESS, GET_MOVIES_ERROR } from '../actionTypes';
 
-function* loadMovies(action) {
-  const { page, filter } = action.payload;
-  let sortBy = '';
+const getSortFilter = (filter) => {
+  let sortBy;
+
   NAV_LINKS.forEach((link) => {
     if (link.value === filter) {
       sortBy = link.filter;
     }
   });
+
+  return sortBy;
+};
+
+function* loadMovies(action) {
+  const { page, filter } = action.payload;
+
   try {
     const { movies, totalPages } = yield call(moviesApi.get, {
       page,
       perPage: MOVIES_PARAMS.PER_PAGE,
-      sortBy,
+      sortBy: getSortFilter(filter),
     });
     yield put({ type: GET_MOVIES_SUCCESS, payload: { movies, totalPages } });
   } catch (e) {
