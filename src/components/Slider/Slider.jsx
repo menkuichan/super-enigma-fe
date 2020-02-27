67e76f5517731c0d3ed329b32b8eef7e78d1519c
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-27 12:14:31 +0300

    refactor slider props

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,27 +7,38 @@ import {
   InputValue,
 } from './styles';
 
-const Slider = ({ value, onChange }) => (
-  <SliderContainer>
-    <div>
-      <InputValue>
-        {`>${value}`}
-      </InputValue>
-    </div>
-    <InputContainer>
-      <SliderInput
-        type="range"
-        min="1"
-        max="10"
-        step="0.1"
-        value={value}
-        onChange={onChange}
-      />
-    </InputContainer>
-  </SliderContainer>
-);
+const Slider = ({
+  value, onChange, min, max, step,
+}) => (
+    <SliderContainer>
+      <div>
+        <InputValue>
+          {`>${value}`}
+        </InputValue>
+      </div>
+      <InputContainer>
+        <SliderInput
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={onChange}
+        />
+      </InputContainer>
+    </SliderContainer>
+  );
+
+Slider.defaultProps = {
+  min: '0',
+  max: '10',
+  step: '0.1',
+};
 
 Slider.propTypes = {
+  min: PropTypes.string,
+  max: PropTypes.string,
+  step: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
