646585c5936641bed467955b6489f193a2d38122
IlonaMenkui <ilonaspolding@gmail.com>
2020-02-28 10:51:12 +0300

    code refactoring

--- src/store/reducers/genres.js
+++ src/store/reducers/genres.js
@@ -15,22 +15,17 @@ const defaultState = {
 
 export const genres = (state = defaultState, { type, payload }) => {
   switch (type) {
-    case GET_GENRES_PENDING:
-      return { ...state };
     case GET_GENRES_SUCCESS:
-      return {
-        ...state,
-        byId: normalizeData(payload.genres),
-      };
-    case GET_GENRES_ERROR:
-      return { ...state };
-    case GET_GENRES_BY_IDS_PENDING:
-      return { ...state, isLoading: true };
     case GET_GENRES_BY_IDS_SUCCESS:
       return {
         ...state,
-        byId: normalizeData(payload.genres),
+        byId: { ...state.byId, ...normalizeData(payload.genres) },
+        isLoading: false,
       };
+    case GET_GENRES_PENDING:
+    case GET_GENRES_BY_IDS_PENDING:
+      return { ...state, isLoading: true };
+    case GET_GENRES_ERROR:
     case GET_GENRES_BY_IDS_ERROR:
       return { ...state, isLoading: false };
     default:
