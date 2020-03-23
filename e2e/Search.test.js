8211854c374ad7613e6565358bb8e547e0515661
IlonaMenkui <ilonaspolding@gmail.com>
2020-03-23 20:15:10 +0300

    SEF-29(feat): Create class for Search page, refactor e2e test for search

--- e2e/Search.test.js
+++ e2e/Search.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-undef */
+import { Search } from './pages/Search';
+
 const puppeteer = require('puppeteer');
-const regeneratorRuntime = require('@babel/runtime/regenerator');
 
 let browser;
 let page;
+let searchPage;
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
@@ -11,41 +13,25 @@ beforeAll(async () => {
     slowMo: 0,
   });
   page = await browser.newPage();
+  searchPage = new Search(page);
 });
 
 describe('Search', () => {
-  const moviesURL = 'http://localhost:8888/movies';
-  const searchInputSelector = '[data-testid="searchInput"]';
-  const movieCardSelector = '[data-testid="movieCard"]';
-  const searchItemSelector = '[data-testid="searchItem"]';
-  const noResultsSelector = '[data-testid="noResults"]';
-
   it('searches correctly with the correct search value', async () => {
-    await page.goto(moviesURL);
-    await page.waitForSelector(searchInputSelector);
-    const searchInput = await page.$(searchInputSelector);
-    await searchInput.tap();
-    await searchInput.type('world');
-    await page.waitForSelector(searchItemSelector);
-    const searchItem = await page.$$(searchItemSelector);
-    expect(searchItem.length).toBeGreaterThanOrEqual(1);
-    await page.keyboard.press('Enter');
-    await page.waitForSelector(movieCardSelector);
-    const movieCard = await page.$$(movieCardSelector);
+    await searchPage.search('world');
+    const searchItems = await searchPage.getSearchItems();
+    expect(searchItems.length).toBeGreaterThanOrEqual(1);
+    await searchPage.enterPress();
+    const movieCard = await searchPage.getMovieCard();
     expect(movieCard.length).toBeGreaterThanOrEqual(1);
   }, 10000);
 
   it('searches correctly with invalid search value', async () => {
-    await page.goto(moviesURL);
-    await page.waitForSelector(searchInputSelector);
-    const searchInput = await page.$(searchInputSelector);
-    await searchInput.tap();
-    await searchInput.type('vjmklolnjbvgfch');
-    const searchItem = await page.$(searchItemSelector);
+    await searchPage.search('vjmklolnjbvgfch');
+    const searchItem = await searchPage.getSearchItem();
     expect(searchItem).toBeNull();
-    await page.keyboard.press('Enter');
-    await page.waitForSelector(noResultsSelector);
-    const noResults = await page.$(noResultsSelector);
+    await searchPage.enterPress();
+    const noResults = await searchPage.getNoResults();
     expect(noResults).toBeTruthy();
   }, 10000);
 });
---
+++ e2e/pages/Search.js
@@ -0,0 +1,49 @@
+const regeneratorRuntime = require('@babel/runtime/regenerator');
+
+export class Search {
+  constructor(page) {
+    this.page = page;
+    this.moviesURL = 'http://localhost:8888/movies';
+    this.selectors = {
+      movieCardSelector: '[data-testid="movieCard"]',
+      searchItemSelector: '[data-testid="searchItem"]',
+      noResultsSelector: '[data-testid="noResults"]',
+      searchInputSelector: '[data-testid="searchInput"]',
+    };
+  }
+
+  async getSearchItems() {
+    await this.page.waitForSelector(this.selectors.searchItemSelector);
+    const searchItems = await this.page.$$(this.selectors.searchItemSelector);
+    return searchItems;
+  }
+
+  async getSearchItem() {
+    const searchItem = await this.page.$(this.selectors.searchItemSelector);
+    return searchItem;
+  }
+
+  async getMovieCard() {
+    await this.page.waitForSelector(this.selectors.movieCardSelector);
+    const movieCard = await this.page.$$(this.selectors.movieCardSelector);
+    return movieCard;
+  }
+
+  async getNoResults() {
+    await this.page.waitForSelector(this.selectors.noResultsSelector);
+    const noResults = await this.page.$(this.selectors.noResultsSelector);
+    return noResults;
+  }
+
+  async enterPress() {
+    await this.page.keyboard.press('Enter');
+  }
+
+  async search(value) {
+    await this.page.goto(this.moviesURL);
+    await this.page.waitForSelector(this.selectors.searchInputSelector);
+    const searchInput = await this.page.$(this.selectors.searchInputSelector);
+    await searchInput.tap();
+    await searchInput.type(value);
+  }
+}
